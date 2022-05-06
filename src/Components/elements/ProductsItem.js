58364bd32f9629c474fc32aa6539c1272f8ec683
Webmaster019 <kingbestd030@gmail.com>
2022-05-06 12:43:50 +0200

    fix projet

--- src/Components/elements/ProductsItem.js
+++ src/Components/elements/ProductsItem.js
@@ -1,28 +1,34 @@
 import React from "react";
-import {ShoppingCart} from "react-feather";
 
-const ProductsItem=({image,titre,prix,onClick,courte_desc})=>{
+const ProductsItem=({image,titre,prix,onClick,courte_desc,prix_solde})=>{
+    const getDiscout=(prix,discount)=>{
+        if (prix && discount){
+            var discountP=(discount * 100)/prix
+            return 100-discountP
+        }else {
+            return 0;
+        }
+    }
     return(
         <div className="col-xl-3 col-lg-3 col-6  mt-4 " onClick={onClick}>
             <div className="cardp">
-                <div className="d-flex itemtop align-items-center justify-content-between px-2 ">
-                    <span className=" fw-bold">{prix}$</span>
-                  {/*  <button className="btn-add">
-                        <ShoppingCart color="#ffffff"/>
-                    </button>*/}
+                {
+                    prix_solde > 0
+                        ?
+                    <div className="d-flex itemtop2 align-items-center justify-content-between px-2 ">
+                    <span className=" fw-bold">{prix}$ - {getDiscout(prix,prix_solde)} %</span>
                 </div>
+                    :
+                    <div className="d-flex itemtop align-items-center justify-content-between px-2 ">
+                    <span className=" fw-bold">{prix}$</span>
+                </div>
+                }
                 <div className="text-center overflow-hidden imgcont ">
                     <img src={process.env.REACT_APP_BASE_URL+"/img/"+image} className="img-fluid"  alt="imageprod"/>
                 </div>
                 <div className="px-2">
-                    <div>
-                        <button className="btn-add itemButton">
-                            <ShoppingCart color="#ffffff"/>
-                        </button>
-                    </div>
                     <span className="titreProd">{titre}</span><br/>
                     <span className="descProduct">{courte_desc.substring(0,80)}...</span>
-                 `
                 </div>
             </div>
         </div>
